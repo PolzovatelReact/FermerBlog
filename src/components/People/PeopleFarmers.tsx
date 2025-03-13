24903e808532ca96f40fd984f9db14114cecdb56
Ruslan Con <wkodojo@gmail.com>
2025-03-13 09:10:57 +0300

    Контрольный commit

--- src/components/People/PeopleFarmers.tsx
+++ src/components/People/PeopleFarmers.tsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
+import { useSelector } from "react-redux";
 import { fetchFarmers } from "../../store/people/farmersSlice";
+import { RootState } from "../../store";
 import { Link } from "react-router-dom";
+import people from "./people.module.css";
+import "../../index.css"; // здесь переключатель темы
+
 //farm_type - напрвления фермы
 const PeopleFarmers: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -11,6 +16,7 @@ const PeopleFarmers: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedDirection, setSelectedDirection] = useState("");
   const [selectedCity, setSelectedCity] = useState("");
+  const theme = useSelector((state: RootState) => state.theme.theme); // Переключение темы
 
   useEffect(() => {
     dispatch(fetchFarmers());
@@ -38,51 +44,64 @@ const PeopleFarmers: React.FC = () => {
   if (hasError) return <p> Ошибка</p>;
   return (
     <div>
-      <h1>Люди</h1>
-      <input
-        type="text"
-        placeholder="Введите имя для поиска"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value.trim())} // trim удаляем пустой пробел
-      />
-      <select
-        value={selectedDirection}
-        onChange={(e) => setSelectedDirection(e.target.value)}
-        style={{ marginBottom: "20px", padding: "10px", width: "100%" }}
-      >
-        <option value="">Все направления</option>
-        {uniqueDirections.map((direction) => (
-          <option key={direction} value={direction}>
-            {direction}
-          </option>
-        ))}
-      </select>
+      <div className={people.content_main}>
+        <div className={people.content_main_left}>Left banner</div>
+        <div className={`${people.content_main_people} navTest ${theme}`}>
+          <h1>Фермеры</h1>
+          <div>
+            {filteredUsers.length > 0 ? (
+              <ul>
+                {filteredUsers.map((user) => (
+                  <li className={` ${people.user_block} `} key={user.id}>
+                    <div></div>
+                    <Link to={`/farmers/${user.id}`}>
+                      {user.username}
+                    </Link> - <p>Направление: {user.farm_type}</p>
+                    <p>Город: {user.location}</p>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>Пользователи не найдены</p>
+            )}
+          </div>
+        </div>
 
-      {/* Выпадающий список для выбора города */}
-      <select
-        value={selectedCity}
-        onChange={(e) => setSelectedCity(e.target.value)}
-        style={{ marginBottom: "20px", padding: "10px", width: "100%" }}
-      >
-        <option value="">Все города</option>
-        {uniqueCities.map((city) => (
-          <option key={city} value={city}>
-            {city}
-          </option>
-        ))}
-      </select>
-      {filteredUsers.length > 0 ? (
-        <ul>
-          {filteredUsers.map((user) => (
-            <li key={user.id}>
-              <Link to={`/farmers/${user.id}`}>{user.username}</Link> -{" "}
-              {user.farm_type} -{user.location}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Пользователи не найдены</p>
-      )}
+        <div className={`${people.content_main_right} navTest ${theme}`}>
+          <input
+            type="text"
+            placeholder="Введите имя для поиска"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value.trim())} // trim удаляем пустой пробел
+          />
+          <select
+            value={selectedDirection}
+            onChange={(e) => setSelectedDirection(e.target.value)}
+            style={{ marginBottom: "20px", padding: "10px", width: "100%" }}
+          >
+            <option value="">Все направления</option>
+            {uniqueDirections.map((direction) => (
+              <option key={direction} value={direction}>
+                {direction}
+              </option>
+            ))}
+          </select>
+
+          {/* Выпадающий список для выбора города */}
+          <select
+            value={selectedCity}
+            onChange={(e) => setSelectedCity(e.target.value)}
+            style={{ marginBottom: "20px", padding: "10px", width: "100%" }}
+          >
+            <option value="">Все города</option>
+            {uniqueCities.map((city) => (
+              <option key={city} value={city}>
+                {city}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
     </div>
   );
 };
